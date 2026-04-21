4c791e6c6db62d3defbc865dacbcffd6858032e7
Sebbi Salama <sebbisalama123@gmail.com>
2026-04-21 03:08:48 +0300

    Updated Application Navigation list

--- app/src/main/java/com/ndejje/saccomobileapplication/SaccoApplication.kt
+++ app/src/main/java/com/ndejje/saccomobileapplication/SaccoApplication.kt
@@ -5,7 +5,15 @@ import com.ndejje.saccomobileapplication.model.AppDatabase
 import com.ndejje.saccomobileapplication.model.SaccoRepository
 
 class SaccoApplication : Application() {
+
+    val database by lazy { AppDatabase.getInstance(this) }
+
+    val repository by lazy {
         SaccoRepository(
+            userDao          = database.userDao(),
+            memberAccountDao = database.memberAccountDao(),
+            transactionDao   = database.transactionDao(),
+            loanRequestDao   = database.loanRequestDao()
         )
     }
 }
